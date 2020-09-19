a9e6defaf417556f2b43efb5964a4fa2e486aaf6
Dakad <3106338+Dakad@users.noreply.github.com>
2020-09-19 22:57:10 +0200

    Add /books route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'home', to: "pages#home", as: "home"
   get 'yello(/:user_name)', to: "pages#yello", as: "yello"
 
+  get "books", to: "books#index", as: "books"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/books_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Books", type: :request do
+
+  describe "GET /books" do
+    it "returns http success" do
+      get "/books/"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
