c5eada5b41f69b8cbc0158c9eeb425082ba00202
Dakad <3106338+Dakad@users.noreply.github.com>
2020-09-21 00:50:01 +0200

    Add simple Model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
+  validates_presence_of :title, :language, :link
+  validates :pages, numericality: { only_integer: true }
+  validates :year, numericality: { only_integer: true }
 
 end
